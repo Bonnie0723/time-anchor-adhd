34e33045b0cafc1ac3bf8e7796a32142ef0ea2da
Bonnie <bonniexwj@gmail.com>
2026-09-08 17:08:59 +0800

    Bump cache for ONCASE palette

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='focus-plan-v6-bw-pixel';
+const CACHE='focus-plan-v7-oncase-pixel';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
