ef4b0b37656b21ade7191d29a4798bb738db531d
Bonnie <bonniexwj@gmail.com>
2026-09-08 16:52:04 +0800

    Bump cache for black white pixel UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='focus-plan-v5-pixel';
+const CACHE='focus-plan-v6-bw-pixel';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
